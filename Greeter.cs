6946b6d8216a48430a4341b1964975bca7b23da4
Adam <fiakkasa@live.com>
2025-11-22 17:25:56 +0200

    Greeter - Use panel for result - Validate input and provide feedback - Clear console per iteration

--- Greeter.cs
+++ Greeter.cs
@@ -1,36 +1,59 @@
 #:package Spectre.Console@0.54.0
 using Spectre.Console;
 
-var _nl = Environment.NewLine;
+const int _minNameLength = 2;
 
 AnsiConsole.MarkupLine(
-    $"[steelblue1]Hello[/] [navy]and[/] [teal]welcome[/] [steelblue1]to[/] [navy]Greeter![/]{_nl}"
+    "[steelblue1]Hello[/] [navy]and[/] [teal]welcome[/] [steelblue1]to[/] [navy]Greeter![/]"
 );
 
 var name = string.Empty;
-string[] questionColors =
+(string, string)[] questionColors =
 [
-    "darkseagreen3",
-    "darkorange3",
-    "purple"
+    ("darkseagreen3", "seagreen3"),
+    ("darkorange3", "orange1"),
+    ("purple", "mediumpurple")
 ];
 
 for (var i = 0; i < questionColors.Length; i++)
 {
-    var color = questionColors[i];
-    AnsiConsole.Markup($"[{color}]Please enter your name: [/]");
-    name = Console.ReadLine()?.Trim() ?? string.Empty;
+    Console.Clear();
 
-    if (name.Length > 0)
+    var (promptColor, validationColor) = questionColors[i];
+
+    if (i > 0)
+    {
+        AnsiConsole.MarkupLine(
+            $"[{validationColor}]Consider using 2 or more characters for your name...[/]"
+        );
+    }
+
+    name = AnsiConsole.Prompt(
+        new TextPrompt<string>($"[{promptColor}]Please enter your name: [/]")
+            .AllowEmpty()
+    );
+
+    if (name.Length >= _minNameLength)
     {
         break;
     }
 }
 
+Console.Clear();
+
 var result = name.Length switch
 {
-    > 0 => $"{_nl}[yellow]Nice to meet you, [bold]{name}[/]![/]",
-    _ => $"{_nl}[silver]Well.. I guess without a name a greeting cannot take place...[/]"
+    >= _minNameLength => $"[yellow]Nice to meet you, [bold]{name}[/]![/]",
+    _ => """ 
+[silver]Well.. I guess without a valid name a greeting cannot take place...[/]
+
+[grey93]Nice to meet you anyway :folded_hands:[/]
+"""
+};
+var panel = new Panel(result)
+{
+    Border = BoxBorder.Rounded,
+    Padding = new Padding(3, 1, 3, 1)
 };
 
-AnsiConsole.MarkupLine(result);
+AnsiConsole.Write(panel);
